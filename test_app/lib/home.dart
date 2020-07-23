4dc650c65a2b8687d42b9f1529efab7a74bb35a8
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-07-23 13:54:04 +0530

    Created Intiial design!

--- test_app/lib/home.dart
+++ test_app/lib/home.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 // Creating the Main function which the runAPP will run!
-FirstApp() {
+firstApp() {
+  // ignore: non_constant_identifier_names
   var FA = Text(
     "Mr. Harshit Dawar",
     textDirection: TextDirection.ltr,
     textAlign: TextAlign.center,
   );
+
+  var design = MaterialApp(
+    home: FA,
+  );
+
+  return design;
 }

22a5beebbfc504ca1b995fb4b75149efc29b1db4
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-07-27 16:34:04 +0530

    Used the column functionality!

--- hot_app/lib/Profile.dart
+++ hot_app/lib/Profile.dart
@@ -1,30 +1,57 @@
 import 'package:flutter/material.dart';
 
 // ignore: non_constant_identifier_names
-var MyBody = Container(
-  width: 500,
-  height: 500,
-  // color: Colors.orange,
-  child: Text(
-    "Welcome to Harshit Dawar's App!",
-    style: TextStyle(
+MyProfile() {
+  var MyBody = Container(
+    width: 500,
+    height: 500,
+    // color: Colors.orange,
+    child: Column(
+      children: <Widget>[
+        Text(
+          "Column Widgets Test",
+          style: TextStyle(
+            fontSize: 23,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        Image.network(
+          "https://github.com/HarshitDawar55/Flutter/raw/master/Images/medium.jpg",
+          fit: BoxFit.cover,
+          filterQuality: FilterQuality.high,
+        ),
+        Text(
+          "Column Widget Test2",
+          style: TextStyle(
+            fontSize: 33,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ),
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
       color: Colors.greenAccent,
-      fontWeight: FontWeight.bold,
-      fontSize: 23,
+      borderRadius: BorderRadius.circular(15),
+      border: Border.all(
+        width: 10,
+        color: Colors.orange,
+      ),
     ),
-  ),
-  alignment: Alignment.center,
-  decoration: BoxDecoration(
-    image: DecorationImage(
-      fit: BoxFit.cover,
-      image: NetworkImage(
-          "https://github.com/HarshitDawar55/Flutter/raw/master/Images/medium.jpg"),
+  );
+
+  // ignore: non_constant_identifier_names
+  var MyHome = Scaffold(
+    appBar: AppBar(
+      backgroundColor: Colors.brown,
+      title: Text("Harshit Dawar's Hot App!!!!!"),
     ),
-    color: Colors.greenAccent,
-    borderRadius: BorderRadius.circular(15),
-    border: Border.all(
-      width: 10,
-      color: Colors.orange,
+    body: Center(
+      child: MyBody,
     ),
-  ),
-);
+  );
+
+  return MyHome;
+}
--- hot_app/lib/home.dart
+++ hot_app/lib/home.dart
@@ -3,18 +3,7 @@ import 'Profile.dart';
 
 // ignore: non_constant_identifier_names
 MyApp() {
-  // ignore: non_constant_identifier_names
-  var MyHome = Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.brown,
-      title: Text("Harshit Dawar's Hot App!!!!!"),
-    ),
-    body: Center(
-      child: MyBody,
-    ),
-  );
-
   return MaterialApp(
-    home: MyHome,
+    home: MyProfile(),
   );
 }

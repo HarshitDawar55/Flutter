23c91a0a325b5a4835d9ba2d4696cfe59ac38746
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-07-27 16:05:08 +0530

    Added Box Decoration!

--- hot_app/lib/home.dart
+++ hot_app/lib/home.dart
@@ -2,12 +2,29 @@ import 'package:flutter/material.dart';
 
 // ignore: non_constant_identifier_names
 MyApp() {
+  var MyBody = Container(
+    width: 500,
+    height: 500,
+    // color: Colors.orange,
+    child: Text("Welcome to Harshit Dawar's App!"),
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+      color: Colors.greenAccent,
+      borderRadius: BorderRadius.circular(15),
+      border: Border.all(
+        width: 10,
+        color: Colors.orange,
+      ),
+    ),
+  );
+
   // ignore: non_constant_identifier_names
   var MyHome = Scaffold(
     appBar: AppBar(
-      backgroundColor: Colors.amber,
-      title: Text("Harshit Dawar's Hot App!!!"),
+      backgroundColor: Colors.brown,
+      title: Text("Harshit Dawar's Hot App!!!!!"),
     ),
+    body: MyBody,
   );
 
   return MaterialApp(

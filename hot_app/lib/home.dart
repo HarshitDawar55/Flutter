e1c3204dd94ea0b8e9ccdbeed5b76804c6fb20c1
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-07-27 15:05:42 +0530

    Added Hot Reload and Module Functionality!

---
+++ hot_app/lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// ignore: non_constant_identifier_names
+MyApp() {
+  // ignore: non_constant_identifier_names
+  var MyHome = Scaffold(
+    appBar: AppBar(
+      backgroundColor: Colors.amber,
+      title: Text("Harshit Dawar's Hot App!!!"),
+    ),
+  );
+
+  return MaterialApp(
+    home: MyHome,
+  );
+}

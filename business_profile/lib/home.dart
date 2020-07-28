8bc147aba681add5b746ced3b3da73d7c6833a31
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-07-28 12:56:44 +0530

    Added AppBar!

--- business_profile/lib/home.dart
+++ business_profile/lib/home.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 
 myHome() {
+  // ignore: non_constant_identifier_names
   var HomeDesign = Scaffold(
     appBar: AppBar(
       backgroundColor: Colors.greenAccent,
       title: Text(
-        "Mr. Harshit Dawar's business Card",
+        "Mr. Harshit Dawar's business Card!",
+        textAlign: TextAlign.center,
         style: TextStyle(
-          fontSize: 25,
+          fontSize: 23,
           fontWeight: FontWeight.bold,
           fontStyle: FontStyle.italic,
-          backgroundColor: Colors.pinkAccent,
+          color: Colors.red,
         ),
       ),
     ),
   );
 
   return MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomeDesign,
   );
 }

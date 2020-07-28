006fc19ab4908082623609013785603d8c9f794d
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-07-28 13:25:52 +0530

    Added nested alignment for the containers!

--- business_profile/lib/home.dart
+++ business_profile/lib/home.dart
@@ -6,19 +6,23 @@ myHome() {
     height: double.infinity,
     color: Colors.amberAccent[100],
     margin: EdgeInsets.all(15),
+    alignment: Alignment.center,
     child: Stack(
-      alignment: Alignment.center,
+      // This will allign the child container as the Top Center.
+      alignment: Alignment.topCenter,
       children: <Widget>[
         Container(
+          // alignment: Alignment.bottomRight,
           width: 300,
-          height: 300,
+          height: 250,
           color: Colors.pinkAccent[100],
         ),
         Container(
-          width: 200,
-          height: 200,
+          width: 150,
+          height: 150,
           color: Colors.brown,
-          margin: EdgeInsets.all(20),
+          // margin: EdgeInsets.all(20),
+          alignment: Alignment.center,
         )
       ],
     ),

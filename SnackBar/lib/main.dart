4afda5bbb2d9c6d540be4ddf420fa0a46df9815b
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-30 14:45:08 +0530

    Started Building the Main APP

--- SnackBar/lib/main.dart
+++ SnackBar/lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(app)
+void main() => runApp(SnackBar_APP());
+
+// ignore: camel_case_types
+class SnackBar_APP extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "SnackBar App",
+          ),
+        ),
+      ),
+    );
+  }
 }
+

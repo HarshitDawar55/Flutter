c24eacc6fba6435eaf8890825738825633d13073
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-30 15:38:02 +0530

    Added comments for layers

--- SnackBar/lib/body.dart
+++ SnackBar/lib/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// child layer of the Program
 class MyBody extends StatefulWidget{
   @override
   MyBodyState createState() => MyBodyState();
@@ -8,8 +9,13 @@ class MyBody extends StatefulWidget{
 class MyBodyState extends State<MyBody>{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return Container(
+      alignment: Alignment.center,
+      child: RaisedButton(
+        color: Colors.greenAccent,
+        child: Text("Harshit Dawar"),
+        onPressed: () => print("HD"),
+        ),
     );
   }
 }
--- SnackBar/lib/main.dart
+++ SnackBar/lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(SnackBar_APP());
 
 // ignore: camel_case_types
+// Parent layer of the Program
 class SnackBar_APP extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.fallback(),
       home: Scaffold(
         appBar: AppBar(

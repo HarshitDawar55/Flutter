2d45c370c388fa56f074729415755f935c91afa5
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-30 14:56:05 +0530

    Created one body File

---
+++ SnackBar/lib/body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyBody extends StatefulWidget{
+  @override
+  MyBodyState createState() => MyBodyState();
+}
+
+class MyBodyState extends State<MyBody>{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
